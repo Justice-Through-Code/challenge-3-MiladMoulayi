7a00abeb6e146d409659bf8998dc06715ae4bb0e
Milad Moulayi <m.moulayi123@gmail.com>
2022-11-19 14:42:19 -0800

    Updated stocks.py

--- stocks.py
+++ stocks.py
@@ -1,10 +1,13 @@
 
 def stock_purchases():
-    amazon = 3000
-    apple = 100
-    fb = 250
-    google = 1400
-    msft = 200
+
+    stocks = {
+        'Amazon': 3000,
+        'Apple': 100,
+        'Facebook': 250,
+        'Google': 1400,
+        'Microsoft': 200
+    }
 
     # Given the prices above and a client's investment budget, how much stock can they buy?
     # 1.1 TODO: Ask the client's name (use the string: "What is your name? ") and save it into a variable
@@ -17,24 +20,27 @@ def stock_purchases():
 
     # 1.3 TODO: Uncomment the line below to ask the client which stock they're interested in.
     # NOTE: Take a look at how this input string prints out
-    stock_name = input("\nWhich stock are you interested in? Enter the full name:\nAmazon\nApple\nFacebook\nGoogle\nMicrosoft\nStock Name: ")
-    
+    stock_name = input(
+        "\nWhich stock are you interested in? Enter the full name:\nAmazon\nApple\nFacebook\nGoogle\nMicrosoft\nStock Name: ")
+
     # Now that you have all of the client info, you can check how much they can purchase of the stock
     # they're interested in.
 
     # 1.4 TODO: Use `if/elif/else` conditional logic to determine how much stock the client can buy,
     # and save it in a variable
-    
-    if (stock_name == "Amazon"):
-      price = amazon
-    elif (stock_name == "Apple"):
-      price = apple
-    elif (stock_name == "Facebook"):
-      price = fb
-    elif (stock_name == "Google"):
-      price = investment / google
-    elif (stock_name == "Microsoft"):
-      price = msft
+
+    # if (stock_name == "Amazon"):
+    #   price = amazon
+    # elif (stock_name == "Apple"):
+    #   price = apple
+    # elif (stock_name == "Facebook"):
+    #   price = fb
+    # elif (stock_name == "Google"):
+    #   price = investment / google
+    # elif (stock_name == "Microsoft"):
+    #   price = msft
+
+    price = stocks[stock_name]
 
     max_stock = int(investment / price)
 
@@ -43,6 +49,5 @@ def stock_purchases():
     # Alex has $5000 to invest and can buy 50 shares of Apple at the current price of $100.
     print(f'{client_name} has ${investment} to invest and can buy {max_stock} shares of {stock_name} at the current price of ${price}.')
 
-# stock_purchases()
 
-
+stock_purchases()
